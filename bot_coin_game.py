d6c00e14f9d6b11039911aa41b7f2f6f6b3c4cfd
Andrei Vladislav <notdquzy228@gmail.com>
2025-01-10 15:51:14 +0500

    Two games

---
+++ bot_coin_game.py
@@ -0,0 +1,13 @@
+import random
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "ОРЕЛ"
+    else:
+        return "РЕШКА"
